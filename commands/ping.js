b30b32d6e4ac1708c6afadc78becde01bf0605a8
jds1985 <jimsaenz1985@yahoo.com>
2025-09-25 11:47:23 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,17 +4,7 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('ping')
     .setDescription('Replies with Pong!'),
-
   async execute(interaction) {
-    try {
-      await interaction.reply('🏓 Pong!');
-    } catch (error) {
-      console.error('❌ Error in ping command:', error);
-      if (interaction.replied || interaction.deferred) {
-        await interaction.followUp({ content: 'There was an error running this command.', ephemeral: true });
-      } else {
-        await interaction.reply({ content: 'There was an error running this command.', ephemeral: true });
-      }
-    }
+    await interaction.reply('🏓 Pong!');
   },
 };
